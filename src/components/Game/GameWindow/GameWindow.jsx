2af2adc99a842b76ff7a0c4e0dc195ed473dadc5
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-31 20:39:07 +0600

    Move ball function added

--- src/components/Game/GameWindow/GameWindow.jsx
+++ src/components/Game/GameWindow/GameWindow.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react"
+import { Context } from "../../../context/Context"
 import "./GameWindow.css"
 
 const GameWindow = () => {
-  return (
-    <div className="Game__window">
-        GameWindow
-        <div className="Game__ball">GameBall</div>
-    </div>
-  )
+    const { count } = useContext(Context)
+
+    const gameBall = {
+        left: `${count}px`,
+        bottom: `${count}px`
+    }
+
+    return (
+        <div className="Game__window">
+            GameWindow
+            <div className="Game__ball" style={gameBall}>GameBall</div>
+        </div>
+    )
 }
 export default GameWindow
